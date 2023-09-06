208314644528796cfe5a74a1f9dccb12a7815d1b
Alquba <140147238+Alquba@users.noreply.github.com>
2023-09-06 16:47:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+const header = document.getElementById("header");
+const menuicon = document.getElementById("menuicon");
+
+
+
+function menuClose(){
+    header.style.display = "none"
+}
+function menuOpen(){
+    header.style.display = "block"
+}
